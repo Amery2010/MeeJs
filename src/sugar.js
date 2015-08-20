2ca6113d2358b6a90c3b67ef791f3614947a5b52
Amery <amery@xiangfa.org>
2015-08-20 18:35:07 +0800

    建立SugarJs的雏形

--- src/sugar.js
+++ src/sugar.js
@@ -8,11 +8,11 @@
  * @version v0.1.0
  */
 
-;(function (Global) {
+;(function (window, document, undifined) {
     'use strict';
 
-    var class2type, type, map, each, query, toArray,
-        isWindow, isDocument, isFunction, isObject,
+    var $, Sugar, class2type, type, map, each, query, toArray,
+        isSugar, isWindow, isDocument, isUndifined, isNull, isElement, isFunction, isObject, isArrayLike, isString,
         emptyArray = [],
         slice = emptyArray.slice;
 
@@ -26,7 +26,11 @@
     }());
 
     type = function type(obj) {
-        return class2type[Object.toString.call(obj)] || 'object';
+        return class2type[Object.prototype.toString.call(obj)] || 'object';
+    };
+
+    isSugar = function isSugar(obj) {
+        return obj instanceof Sugar;
     };
 
     isWindow = function isWindow(obj) {
@@ -37,12 +41,32 @@
         return obj.nodeType === obj.DOCUMENT_NODE;
     };
 
+    isUndifined = function isUndifined(obj) {
+        return obj === undifined;
+    };
+
+    isNull = function isNull(obj) {
+        return obj !== obj;
+    };
+
+    isElement = function isElement(obj) {
+        return obj.nodeType === 1;
+    };
+
     isObject = function isObject(obj) {
         return type(obj) === 'object';
     };
 
     isFunction = function isFunction(obj) {
         return type(obj) === 'function';
+    };
+
+    isArrayLike = function isArrayLike(obj) {
+        return 'length' in obj && type(obj.length) === 'number';
+    };
+
+    isString = function isString(str) {
+        return type(str) === 'string';
     };
 
     toArray = function toArray(obj) {
@@ -93,34 +117,79 @@
         return toArray(context.querySelectorAll(selector));
     };
 
-    function Sugar(context) {
+    var attr = (function () {
+        var method,
+            defaultMethod = function (elem, name, value) {
+                if (isNull(method)) {
+                    if (arguments.length === 1) {
+                        if (isObject(name)) {
+                            method = function (elem, name) {
+                                each(name, function (val, key) {
+                                    elem.setAttribute(key, val);
+                                });
+                            };
+                            method(elem, name);
+                        } else {
+                            return elem.getAttribute(name);
+                        }
+                    } else {
+                        elem.setAttribute(name, isFunction(value) ? value.call(this, elem, elem.getAttribute(name)) : value);
+                    }
+                } else {
+                    method(elem, name, value);
+                }
+            };
+
+        return function (elem, name, value) {
+            var len;
+            if (elem.length === 1) {
+                defaultMethod.call(this[0], name, value);
+            } else {
+                len = elem.length -1;
+                while (len--) {
+                    method(this[len], name, value);
+                }
+                method = null;
+            }
+        };
+    }());
+
+    Sugar = function (context) {
         this.element = context;
-    }
+    };
 
     Sugar.prototype = {
-        attr: function (name, value) {
-            if (arguments.length === 1) {
-                if (isObject(name)) {
-                    each(name, function (val, key) {
-                        this.element.setAttribute(key, val);
-                    }.bind(this));
-                } else {
-                    this.element.getAttribute(name);
-                }
-            } else {
-                this.element.setAttribute(name, value);
-            }
-            return this;
-        },
+        attr: attr,
         removeAttr: function (name) {
             this.element.removeAttribute(name);
             return this;
         }
     };
 
-    Global.$ = function (selector) {
-        return new Sugar(Global.document.querySelector(selector));
+    $ = function (selector, context) {
+        if (isString(selector)) {
+            if (context && isSugar(context)) {
+                return context.find(selector);
+            } else {
+                return new Sugar(selector);
+            }
+        } else if (isSugar(selector)) {
+            return selector;
+        } else if (isArrayLike(selector)) {
+            return toArray(selector);
+        } else if (isObject(selector)) {
+            if (isElement(selector)) {
+                return new Sugar(selector);
+            } else if (isWindow(selector)) {
+                return window;
+            } else if (isDocument(selector)) {
+                return new Sugar(document);
+            }
+        }
     };
 
-    Global.Sugar = Sugar;
-}(window));
+    window.Sugar = $;
+    if (window.$ === undefined) {
+        window.$ = $;
+    }
+}(window, document));
